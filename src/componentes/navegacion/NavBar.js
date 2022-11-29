ee48e76acf5cee8d9688f3c8c212870da866d506
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2022-11-28 19:08:32 -0500

    funcion menu added

--- src/componentes/navegacion/NavBar.js
+++ src/componentes/navegacion/NavBar.js
@@ -1,71 +1,111 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
-import { Bell } from "@easy-eva-icons/react";
-import { Settings2 } from "@easy-eva-icons/react";
-import { LogOut } from "@easy-eva-icons/react";
-import { Person } from "@easy-eva-icons/react";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthProvider";
+import {
+  Archive,
+  Bell,
+  LogIn,
+  People,
+  PersonAdd,
+  ShoppingCart,
+  Settings2,
+  LogOut,
+  Person
+} from "@easy-eva-icons/react";
+import avatar from '../navegacion/avatar1.png';
+import Logo from '../../assets/logo.png';
+import '../navegacion/NavBar.css'
 
 const NavBar = () => {
-    const [open, setOpen] = useState(false)
-    let menuRef = useRef();
+  const [open, setOpen] = useState(false)
+  let menuRef = useRef();
 
-    const toggle = () => {
-        setOpen(!open)
+  const toggle = () => {
+    setOpen(!open)
+  }
+
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false)
+      }
+
+
     }
 
+    document.addEventListener("mousedown", handler);
 
-    useEffect(() => {
-        let handler = (e) => {
-            if (!menuRef.current.contains(e.target)) {
-                setOpen(false)
-            }
+    return () => {
+      document.removeEventListener("mousedown", handler)
 
+    }
 
-        }
+  });
 
-        document.addEventListener("mousedown, handler");
+  const { setAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-        return () => {
-            document.removeEventListener("mousedown", handler)
+  const logout = async () => {
 
-        }
+    setAuth({});
+    navigate('/login');
+  }
 
-    });
-
-
-    return (
-        <header className="Header">
-            <div className="Header-row Container">
-                <div className="Header-column">
-                    <img className="logo" src="https://www.andeslibreria.com/img/cms/andeslibreria.com.png" alt="Anhelo del Saber.com"></img>
-                    <nav className="Header-nav">
-                        <ul className="Header-ul">
-                            <li className="Header-li"><NavLink exact to="/Inicio">Inicio</NavLink></li>
-                            <li className="Header-li"><NavLink exact to="/Productos">Productos</NavLink></li>
-                            <li className="Header-li"><NavLink exact to="/Usuarios">Usuarios</NavLink></li>
-                            <li className="Header-li"><NavLink exact to="/Inventario">Inventario</NavLink></li>
-
-                        </ul>
-                    </nav>
-                </div>
-                <div className="Header-column">
-                    <div className="Header-notificacion">
-                        <Bell className="Header-notificacion--icon" />
-                        <div className="Header-notificacion--punto"></div>
-                    </div>
-                    <div className="Header-avatar">
-                        <img class="avatar-img" src="https://fabrx.co/preview/muse-dashboard/assets/img/avatar1.png" alt="Avatar"></img>
-                        <ul className="Header-avatar-ul">
-                            <li className="Header-avatar-li"> <Person className="Header-avatar--icono" />  <NavLink exact to="/Perfil">Mi  Perfil</NavLink></li>
-                            <li className="Header-avatar-li"> <Settings2 className="Header-avatar--icono" />  <NavLink exact to="/Ajustes">Ajustes</NavLink></li>
-                            <hr className="Header-avatar--hr" />
-                            <li className="Header-avatar-li">  <LogOut className="Header-avatar--icono" />   <NavLink exact to="/Salir">Salir</NavLink></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className="Header">
+      <div className="Header-row Container">
+        <div className="Header-column--left">
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="Header-logo"></img>
+          </Link>
+          <nav className="Header-nav">
+            <ul className="Header-ul">
+              <li className="Header-li">
+                <NavLink to="/login" className="Header-nav--link"><LogIn className="Header-icon--nav" />Login</NavLink>
+              </li>
+              <li className="Header-li">
+                <NavLink to="/register" className="Header-nav--link"><PersonAdd className="Header-icon--nav" />Register</NavLink>
+              </li>
+              <li className="Header-li">
+                <NavLink to="/products" className="Header-nav--link"><Archive className="Header-icon--nav" /> Productos</NavLink>
+              </li>
+              <li className="Header-li">
+                <NavLink to="/usuarios" className="Header-nav--link"><People className="Header-icon--nav" /> Usuarios</NavLink>
+              </li>
+              <li className="Header-li">
+                <NavLink to="/carrito" className="Header-nav--link"><ShoppingCart className="Header-icon--nav" />Carrito</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="Header-column--right">
+          <div className="Header-notificacion">
+            <NavLink to="/notifications">
+              <Bell className="Header-icon" />
+            </NavLink>
+            <div className="Header-notificacion--punto"></div>
+          </div>
+          <div className="Header-usernav" ref={menuRef} onClick={toggle}>
+            <img className="Header-usernav--img" src={avatar} alt="Avatar"></img>
+            <ul className={`Header-popup ${open ? 'isActive' : ''}`}>
+              <li className="Header-popup--li">
+                <NavLink to="/perfil" className="Header-popup--link"> <Person className="Header-icon--link" />Perfil</NavLink>
+              </li>
+              <li className="Header-popup-li">
+                <NavLink to="/ajustes" className="Header-popup--link"> <Settings2 className="Header-icon--link" />Ajustes</NavLink>
+              </li>
+              <hr className="Header-popup--hr" />
+              <li className="Header-popup--li">
+                <button className="Btn-logout" onClick={logout}><LogOut className="Header-icon--link" />Salir</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default NavBar;
---
+++ src/views/Usuarios.js
@@ -0,0 +1,13 @@
+import '../styles/Usuarios.css'
+
+const Usuarios = () => {
+    return (
+        <section className="Usuarios">
+            <div className="Usuarios-wrapper Container">
+                <h1 className="Display">Usuarios</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Usuarios;
